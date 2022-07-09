c1a121ab08b1a43f317829b2f44a8944737b32fa
Yu-Ren Zhang <e8035669@gmail.com>
2022-07-09 21:56:32 +0800

    update AttackMobsTask

--- src/main/java/net/fabricmc/example/AttackMobsTask.java
+++ src/main/java/net/fabricmc/example/AttackMobsTask.java
@@ -6,6 +6,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.ingame.InventoryScreen;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.mob.Monster;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -19,12 +21,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+import java.util.stream.StreamSupport;
 
 import static net.fabricmc.fabric.api.client.command.v2.ClientCommandManager.literal;
 
@@ -60,16 +65,47 @@ public class AttackMobsTask {
         try {
             HotPlugMouse.unplugMouse(client);
 
-            client.execute(() -> client.setScreen(null));
-            Thread.sleep(500);
+            while (true) {
+                client.execute(() -> client.setScreen(null));
+                Thread.sleep(500);
 
-            List<ItemStack> hotBarItems = PlayerSlots.getHotBarItems(client);
-            int[] bowIndex = filterItem(hotBarItems, Items.BOW);
-            if (bowIndex.length == 0) {
-                throw new RuntimeException("There is no bow");
+                List<ItemStack> hotBarItems = PlayerSlots.getHotBarItems(client);
+                int[] bowIndex = filterItem(hotBarItems, Items.BOW);
+                if (bowIndex.length == 0) {
+                    // throw new RuntimeException("There is no bow");
+                    LOGGER.info("There is no bow");
+                    break;
+                }
+
+                int[] arrows = filterItem(hotBarItems, Items.ARROW);
+                if (arrows.length == 0) {
+                    LOGGER.info("There is no arrow");
+                    break;
+                }
+
+                pressKey(client.options.hotbarKeys[bowIndex[0]]);
+
+                Optional<Entity> entity = StreamSupport
+                        .stream(client.world.getEntities().spliterator(), false)
+                        .filter(e -> e instanceof Monster && e.getPos().distanceTo(client.player.getPos()) < 20)
+                        .min(Comparator.comparingDouble(e -> e.getPos().distanceTo(client.player.getPos())));
+
+                if (entity.isPresent()) {
+                    client.options.useKey.setPressed(true);
+                    Thread.sleep(1000);
+
+                    LookDirection lookDirection = new LookDirection(client, entity.get().getEyePos());
+                    playerMotion.addTask(lookDirection);
+
+                    while (!lookDirection.isFinished()) {
+                        Thread.sleep(10);
+                    }
+                    Thread.sleep(20);
+                    client.options.useKey.setPressed(false);
+                } else {
+                    LOGGER.info("Entity not found");
+                }
             }
-
-            pressKey(client.options.hotbarKeys[bowIndex[0]]);
 
         } catch (Exception ex) {
             LOGGER.info(ex);
@@ -105,6 +141,7 @@ public class AttackMobsTask {
         Thread.sleep(200);
         keyBinding.setPressed(false);
     }
+
     private void pressInventoryKey() throws InterruptedException {
         pressKey(client.options.inventoryKey);
     }
